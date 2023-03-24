7727828c2053fbfd42fc25c07e761533e1661ec9
AndreiDudenkov <andrei634d@gmail.com>
2023-03-24 23:57:35 +0300

    User page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+     <div className="title"> <h1>User page</h1> </div>
+        <div>
+            <h2> Resurses</h2>
+        <a href='https://www.youtube.com'>Youtube</a> <br/>
+        <a href='https://www.vk.com'>VK</a> <br/>
+        <a href='https://www.onliner.by'>Onliner</a>
+        </div>
+        </div>
+
   );
 }
 
