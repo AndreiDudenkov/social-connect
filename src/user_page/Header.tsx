28e32c53d8187c6965e9492334aa9b7206592f22
AndreiDudenkov <andrei634d@gmail.com>
2023-03-25 00:57:33 +0300

    User page v.2.1 import/export

---
+++ src/user_page/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+    return (<div>
+            <div className="title"><h1>User page</h1></div>
+            <div>
+                <a href=''>Home</a> <br/>
+                <a href=''>News</a><br/>
+                <a href=''>Messages</a><br/>
+                <a href=''>Settings</a><br/>
+            </div>
+        </div>
+    )
+}
---
+++ src/user_page/Technologies.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <h2> Resurses</h2>
+            <a href='https://www.youtube.com'>Youtube</a> <br/>
+            <a href='https://www.vk.com'>VK</a> <br/>
+            <a href='https://www.onliner.by'>Onliner</a>
+        </div>
+    )
+}
