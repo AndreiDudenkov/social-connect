43b7c6d3055af00190992d07d650b9e059131206
AndreiDudenkov <andrei634d@gmail.com>
2023-05-22 01:04:26 +0300

    Social network external components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+
+export const Header = () => {
+    return (<header className={'header'}>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF1vm3g8JVcKB8VG60WGTckmiIJ-C6XZE89A&usqp=CAU"/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+
+export const Profile = () => {
+    return (<div className={'content'}>
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdgQ9pro7ubhf9a7-MAxZqRM5N6HEJfoNgLw&usqp=CAU"/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+        </div>
+    )
+}
