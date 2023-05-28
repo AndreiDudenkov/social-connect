ad3daf1793f4258534b4e2df0cadef33133cb5b9
AndreiDudenkov <andrei634d@gmail.com>
2023-05-29 01:39:12 +0300

    Social network Router

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './Dialogs.module.css'
+
+
+
+export const Dialogs = () => {
+    return (
+        <div >
+Dialogs
+        </div>
+
+    )
+}
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import s from './Profile.module.css'
 import {MyPosts} from './MyPosts/MyPosts';
 
 
 
 export const Profile = () => {
          return (
-    <div className={s.content}>
+    <div >
             <div>
                 <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdgQ9pro7ubhf9a7-MAxZqRM5N6HEJfoNgLw&usqp=CAU"/>
