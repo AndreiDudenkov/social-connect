9d9472dc697bd42615b68a356a500cca13443808
AndreiDudenkov <andrei634d@gmail.com>
2023-06-06 00:49:04 +0300

    Social network all addPost (button oncklick)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from './MyPosts.module.css'
 import {MyPost} from './Post/MyPost';
 import {PostType} from '../../../App';
@@ -9,20 +9,26 @@ type MyPostsType = {
 }
 
 
-export const MyPosts:React.FC<MyPostsType> = ({posts}) => {
+export const MyPosts: React.FC<MyPostsType> = ({posts}) => {
 
-    const postElement = posts.map(p=><MyPost message={p.message} likes={p.likes}/>)
+    const postElement = posts.map(p => <MyPost message={p.message} likes={p.likes}/>)
 
+    let newPostElement = useRef<HTMLTextAreaElement>(null)
+
+    const addPostHandler = () => {
+         let text = newPostElement.current !== null? newPostElement.current.value: null
+         console.log(text)
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3> My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPostHandler}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
